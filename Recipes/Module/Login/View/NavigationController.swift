281eba50eab2c9bbda6e3c18a58c61b915a4a582
Alaa Gawish <53160167+alaagawish@users.noreply.github.com>
2023-08-05 08:19:37 +0300

    login and save user logic

---
+++ Recipes/Module/Login/View/NavigationController.swift
@@ -0,0 +1,21 @@
+//
+//  NavigationController.swift
+//  Recipes
+//
+//  Created by Alaa on 05/08/2023.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
--- Recipes/Utils/Constants.swift
+++ Recipes/Utils/Constants.swift
@@ -9,5 +9,17 @@ import Foundation
 
 class Constants {
     static let url = "https://api.npoint.io/43427003d33f1f6b51cc"
+    static let navigator = "navigator"
+    static let yes = "Yes"
+    static let ok = "Ok"
+    static let cancel = "Cancel"
+    static let homeIdentifier = "homeIdentifier"
+    static let incorrectInformation = "Incorrect Information"
+    static let tryAgain = "The information you entered is incorrect.\nPlease try again."
+    static let email = "alaa@gmail.com"
+    static let password = "alaa1234"
+    static let userPassword = "password"
+    static let userEmail = "email"
+    
     
 }
