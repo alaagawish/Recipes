2439049bc85697154100863c098007bfda0fdb68
Alaa Gawish <53160167+alaagawish@users.noreply.github.com>
2023-08-06 03:24:00 +0300

    displaying details of recipe

--- Recipes/Module/Details/View/DetailsViewController.swift
+++ Recipes/Module/Details/View/DetailsViewController.swift
@@ -6,25 +6,63 @@
 //
 
 import UIKit
+import Kingfisher
 
 class DetailsViewController: UIViewController {
-
+    
+    @IBOutlet weak var recipeImageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var ingredients: UILabel!
+    @IBOutlet weak var recipeDescriptionLabel: UILabel!
+    @IBOutlet weak var proteins: UILabel!
+    @IBOutlet weak var calories: UILabel!
+    @IBOutlet weak var headline: UILabel!
+    @IBOutlet weak var caloriesTitle: UILabel!
+    @IBOutlet weak var proteinsTitle: UILabel!
     var recipe: Recipe!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setUpUI()
+        checkExistance()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpUI() {
+        name.text = recipe.name
+        proteins.text = recipe.proteins
+        calories.text = recipe.calories
+        headline.text = recipe.headline
+        recipeDescriptionLabel.text = recipe.description
+        let url = URL(string: recipe.image)
+        recipeImageView.kf.setImage(with: url,
+                                    placeholder: UIImage(named: Constants.noImage))
+        for ingredient in recipe.ingredients ?? [] {
+            ingredients.text = (ingredients.text ?? "") + ingredient + "\n"
+            
+        }
+        
     }
-    */
-
+    
+    func checkExistance() {
+        if recipe.calories.isEmpty {
+            calories.isHidden = true
+            caloriesTitle.isHidden = true
+        }else {
+            calories.isHidden = false
+            caloriesTitle.isHidden = false
+        }
+        
+        if ((recipe.proteins?.isEmpty) == true) {
+            proteins.isHidden = true
+            proteinsTitle.isHidden = true
+        }else {
+            proteins.isHidden = false
+            proteinsTitle.isHidden = false
+        }
+    }
+    
+    
+    
 }
